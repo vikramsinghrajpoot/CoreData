50c5ba495dab82dbd9081df94e492c0700b17a5c
vikram rajpoot <ios.vikram@gmail.com>
2017-11-19 17:44:40 +0530

    Initial Commit

---
+++ BankDB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BankDB
+//
+//  Created by vikram singh rajpoot on 19/11/17.
+//  Copyright © 2017 vikram singh rajpoot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
